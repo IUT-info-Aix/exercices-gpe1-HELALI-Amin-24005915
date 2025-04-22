0cb7991e4a8f2658a6815a934fc3f3ebeecb0b21
HELALI <amin.helali@etu.univ-amu.fr>
2025-04-22 17:29:45 +0200

    creation du contenue du millieux

--- src/main/java/fr/amu/iut/exercice1/FenetreLogiciel.java
+++ src/main/java/fr/amu/iut/exercice1/FenetreLogiciel.java
@@ -1,13 +1,11 @@
 package fr.amu.iut.exercice1;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javax.swing.*;
@@ -19,7 +17,7 @@ public class FenetreLogiciel extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Premier exemple manipulant les conteneurs");
         BorderPane root = new BorderPane();
- ///////Menubar
+        //Menubar
         Menu menu1 = new Menu("File");
         MenuItem File1 = new MenuItem("File1");
         MenuItem File2 = new MenuItem("File2");
@@ -40,26 +38,57 @@ public class FenetreLogiciel extends Application {
         MenuBar menuBar = new MenuBar(menu1, menu2, menu3); //on met tout nos menue ici pour ensuite definir leur emplacement avec root
 
         //Boutons a gauche
-        Label boutontext = new Label(" Boutons :");
+        Label boutontext = new Label(" Boutons : ");
         Button bouton1 = new Button("Bouton1");
         Button bouton2 = new Button("Bouton2");
         Button bouton3 = new Button("Bouton3");
 
-        VBox boutongauche = new VBox(boutontext, bouton1, bouton2, bouton3); //on ne peut pas faire comme avec les menu on les met directement dans la VBOX ou Hbox
-        
+
+        VBox boutongauche = new VBox(boutontext, bouton1, bouton2, bouton3);//on ne peut pas faire comme avec les menu on les met directement dans la VBOX ou Hbox
+        boutongauche.setAlignment(Pos.CENTER);
         HBox bareDeBoutons = new HBox(boutongauche, new Separator(Orientation.VERTICAL));
 
-        //Texte a gauche
+
+
+        //TextField aux centre et champ de saisie
+        Label Name = new Label("Name :");
+        Label Email = new Label("Email :");
+        Label Password = new Label("Password :");
+        //champs de saisie
+        TextField Name1 = new TextField();
+        TextField Email1 = new TextField();
+        TextField Password1 = new PasswordField();
+        //création d'un grid ou il faudra mettre la position des objets
+        GridPane gridpane = new GridPane();
+        gridpane.setAlignment(Pos.CENTER);//Le placer au centre
+        gridpane.setHgap(2);
+        gridpane.setVgap(3);
+        //Définition des position des Label
+        gridpane.add(Name, 0, 0);
+        gridpane.add(Email, 0, 1);
+        gridpane.add(Password, 0, 2);
+        //Définition des position des TextField
+        gridpane.add(Name1, 1, 0);
+        gridpane.add(Email1, 1, 1);
+        gridpane.add(Password1, 1, 2);
+
+        //création et positionnement des boutons
+        Button submit = new Button("Submit");
+        Button cancel = new Button("Cancel");
+
+        gridpane.add(submit, 0, 3);
+        gridpane.add(cancel, 1, 3);
+
+
+
+
+
+
+        root.setCenter(gridpane);
+
+        //Texte et boutons a gauche
         root.setLeft(bareDeBoutons);
         root.setTop(menuBar);
-
-
-
-
-
-
-
-
 
 
         VBox vbox = new VBox();
