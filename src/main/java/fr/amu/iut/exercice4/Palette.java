1eec1451b96765eb7097aa2a78f080e91cc6029f
HELALI-Amin-24005915 <amin.helali@etu.univ-amu.fr>
2025-04-26 17:11:15 +0200

    Palett ede couleur terminer

--- src/main/java/fr/amu/iut/exercice4/Palette.java
+++ src/main/java/fr/amu/iut/exercice4/Palette.java
@@ -9,7 +9,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class Palette extends Application {
+public class Palette extends Application  {
 
     private int nbVert = 0;
     private int nbRouge = 0;
