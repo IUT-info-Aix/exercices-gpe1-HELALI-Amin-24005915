5b389f2bc4f4be93dee4dcb84eeb4c0a19ec0a6a
HELALI <amin.helali@etu.univ-amu.fr>
2025-05-06 15:08:54 +0200

    login et mdp, fxml imcomplet

--- src/main/java/fr/amu/iut/exercice8/LoginControl.java
+++ src/main/java/fr/amu/iut/exercice8/LoginControl.java
@@ -1,43 +1,32 @@
 package fr.amu.iut.exercice8;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
-public class LoginControl {
-
-    // Liens vers les composants définis dans le fichier FXML
-    @FXML
-    private TextField usernameField;
+public class LoginControl extends GridPane {
 
     @FXML
-    private PasswordField passwordField;
+    private TextField name;
+    @FXML
+    private PasswordField pwd;
 
     @FXML
-    private Button loginButton;
-
-    @FXML
-    private Button cancelButton;
-
-    // Action associée au bouton Login
-    @FXML
-    private void handleLoginAction() {
-        String username = usernameField.getText();
-        String password = passwordField.getText();
-
-        if (username.isEmpty() || password.isEmpty()) {
-            System.out.println("Error: Both fields are required.");
-        } else {
-            System.out.println("Login successful for user: " + username);
+    private void okClicked() {
+        StringBuilder pw = new StringBuilder();
+        System.out.println(name.getText() + " ");
+        for (int i = 0 ; i < pwd.getText().length(); i++) {
+            pw.append('*');
         }
+        System.out.println(pw);
+
     }
 
-    // Action associée au bouton Cancel
     @FXML
-    private void handleCancelAction() {
-        usernameField.clear();
-        passwordField.clear();
-        System.out.println("Fields cleared.");
+    private void cancelClicked() {
+        name.setText("");
+        pwd.setText("");
     }
 }
