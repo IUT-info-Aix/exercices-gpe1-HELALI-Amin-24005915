5dc40f2b5954241f6cdfdc1d80918d10d3f29ec7
amin Helali <amin.helali@etu.univ-amu.fr>
2025-05-13 17:24:06 +0200

    correction des chemins

--- src/main/java/fr/amu/iut/exercice11/Palette.java
+++ src/main/java/fr/amu/iut/exercice11/Palette.java
@@ -9,7 +9,7 @@ public class Palette extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fr/amu/iut/exercice11/Palette.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Palette.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         primaryStage.setScene(scene);
         primaryStage.setTitle("Palette de couleurs");
