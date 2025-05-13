e436b516834f3fbdf3a7a888453fddbc38ce686d
amin Helali <amin.helali@etu.univ-amu.fr>
2025-05-13 17:10:45 +0200

    correction de l'exercices 16

--- src/main/java/fr/amu/iut/exercice16/ConvertisseurTemperatures.java
+++ src/main/java/fr/amu/iut/exercice16/ConvertisseurTemperatures.java
@@ -2,9 +2,6 @@ package fr.amu.iut.exercice16;
 
 import javafx.application.Application;
 import javafx.beans.binding.Bindings;
-import javafx.beans.binding.NumberBinding;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -13,73 +10,85 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.util.StringConverter;
 import javafx.util.converter.NumberStringConverter;
 
 public class ConvertisseurTemperatures extends Application {
-    private DoubleProperty celsius = new SimpleDoubleProperty(0);
-    private DoubleProperty fahrenheit = new SimpleDoubleProperty(32);
 
     @Override
     public void start(Stage primaryStage) {
         // Création du panneau Celsius
         VBox panneauCelsius = new VBox(10);
-        Label labelCelsius = new Label("Celsius:");
-        TextField textCelsius = new TextField();
-        Slider sliderCelsius = new Slider(-100, 100, 0);
+        Label labelCelsius = new Label("°C");
+        labelCelsius.setStyle("-fx-font-weight: bold");
+        
+        Slider sliderCelsius = new Slider(0, 100, 0);
         sliderCelsius.setShowTickMarks(true);
         sliderCelsius.setShowTickLabels(true);
-        sliderCelsius.setMajorTickUnit(50);
-        sliderCelsius.setMinorTickCount(5);
-        panneauCelsius.getChildren().addAll(labelCelsius, textCelsius, sliderCelsius);
+        sliderCelsius.setMajorTickUnit(10);
+        sliderCelsius.setBlockIncrement(1);
+        sliderCelsius.setPrefHeight(300);
+        sliderCelsius.setOrientation(javafx.geometry.Orientation.VERTICAL);
+        
+        TextField textCelsius = new TextField("0.00");
+        textCelsius.setPrefColumnCount(4);
+        textCelsius.setEditable(true);
+        
+        panneauCelsius.getChildren().addAll(labelCelsius, sliderCelsius, textCelsius);
+        panneauCelsius.setAlignment(javafx.geometry.Pos.CENTER);
         
         // Création du panneau Fahrenheit
         VBox panneauFahrenheit = new VBox(10);
-        Label labelFahrenheit = new Label("Fahrenheit:");
-        TextField textFahrenheit = new TextField();
-        Slider sliderFahrenheit = new Slider(-148, 212, 32);
+        Label labelFahrenheit = new Label("°F");
+        labelFahrenheit.setStyle("-fx-font-weight: bold");
+        
+        Slider sliderFahrenheit = new Slider(32, 212, 32);
         sliderFahrenheit.setShowTickMarks(true);
         sliderFahrenheit.setShowTickLabels(true);
-        sliderFahrenheit.setMajorTickUnit(50);
-        sliderFahrenheit.setMinorTickCount(5);
-        panneauFahrenheit.getChildren().addAll(labelFahrenheit, textFahrenheit, sliderFahrenheit);
+        sliderFahrenheit.setMajorTickUnit(20);
+        sliderFahrenheit.setBlockIncrement(1);
+        sliderFahrenheit.setPrefHeight(300);
+        sliderFahrenheit.setOrientation(javafx.geometry.Orientation.VERTICAL);
+        
+        TextField textFahrenheit = new TextField("32.00");
+        textFahrenheit.setPrefColumnCount(4);
+        textFahrenheit.setEditable(true);
+        
+        panneauFahrenheit.getChildren().addAll(labelFahrenheit, sliderFahrenheit, textFahrenheit);
+        panneauFahrenheit.setAlignment(javafx.geometry.Pos.CENTER);
 
-        // Binding bidirectionnel entre les propriétés et les contrôles
-        // Celsius <-> TextField et Slider
-        StringConverter<Number> converter = new NumberStringConverter();
-        Bindings.bindBidirectional(textCelsius.textProperty(), celsius, converter);
-        sliderCelsius.valueProperty().bindBidirectional(celsius);
+        // Binding bidirectionnel entre les sliders et les textFields
+        Bindings.bindBidirectional(
+            textCelsius.textProperty(),
+            sliderCelsius.valueProperty(),
+            new NumberStringConverter("#0.00")
+        );
         
-        // Fahrenheit <-> TextField et Slider
-        Bindings.bindBidirectional(textFahrenheit.textProperty(), fahrenheit, converter);
-        sliderFahrenheit.valueProperty().bindBidirectional(fahrenheit);
+        Bindings.bindBidirectional(
+            textFahrenheit.textProperty(),
+            sliderFahrenheit.valueProperty(),
+            new NumberStringConverter("#0.00")
+        );
         
-        // Binding de conversion entre Celsius et Fahrenheit
-        // F = C * 9/5 + 32
-        // C = (F - 32) * 5/9
-        NumberBinding celsiusToFahrenheit = celsius.multiply(9.0/5.0).add(32);
-        NumberBinding fahrenheitToCelsius = fahrenheit.subtract(32).multiply(5.0/9.0);
-        
-        // Utilisation de l'astuce du ChangeListener à ne déclencher qu'une seule fois
-        fahrenheit.addListener((obs, oldVal, newVal) -> {
-            if (Math.abs(newVal.doubleValue() - celsiusToFahrenheit.getValue().doubleValue()) > 0.01) {
-                celsius.set(fahrenheitToCelsius.getValue().doubleValue());
-            }
+        // Binding bidirectionnel entre les valeurs Celsius et Fahrenheit
+        // Formule: F = C * 9/5 + 32
+        sliderCelsius.valueProperty().addListener((obs, oldVal, newVal) -> {
+            double celsius = newVal.doubleValue();
+            double fahrenheit = celsius * 9.0/5.0 + 32;
+            sliderFahrenheit.setValue(fahrenheit);
         });
         
-        celsius.addListener((obs, oldVal, newVal) -> {
-            if (Math.abs(newVal.doubleValue() - fahrenheitToCelsius.getValue().doubleValue()) > 0.01) {
-                fahrenheit.set(celsiusToFahrenheit.getValue().doubleValue());
-            }
+        sliderFahrenheit.valueProperty().addListener((obs, oldVal, newVal) -> {
+            double fahrenheit = newVal.doubleValue();
+            double celsius = (fahrenheit - 32) * 5.0/9.0;
+            sliderCelsius.setValue(celsius);
         });
-        
-        // Initialisation
-        celsius.set(0);
 
         HBox root = new HBox(30, panneauCelsius, panneauFahrenheit);
         root.setPadding(new Insets(20));
+        root.setAlignment(javafx.geometry.Pos.CENTER);
 
-        primaryStage.setScene(new Scene(root));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
         primaryStage.setTitle("Convertisseur de températures");
         primaryStage.show();
     }
